386ae743da7adb0c5c05dc97f74cbf53a73e8c1f
Hodu-C <za4758@icloud.com>
2023-11-20 21:50:07 +0900

    first commit

---
+++ src/main/java/com/melog/melog/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.melog.melog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(value="/home")
+    public String home() {
+        return "home";
+    }
+}
