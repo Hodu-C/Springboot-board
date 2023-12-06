784fd0f020b27226e6c8ffd381623d1592ad708d
Hodu-C <za4758@icloud.com>
2023-12-07 00:05:17 +0900

    refactor - Board Domain Board - Controller 불필요 부분 제거 Board - Response 타입 명시, 불필요 응답 데이터 제거 Board - Dto 유효성 검사 추가 Board - Dto 이름 변경

--- src/main/java/com/melog/melog/domain/board/controller/BoardController.java
+++ src/main/java/com/melog/melog/domain/board/controller/BoardController.java
@@ -16,7 +16,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RequiredArgsConstructor
 @RestController
 public class BoardController {
